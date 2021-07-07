ec6b2e1e8725d6d48dd7234b0facaec59c6baf94
LukiBa <e1426506@student.tuwien.ac.at>
2021-07-07 12:41:14 +0000

    add test images + upd test file

--- test-yolov3-tiny.py
+++ test-yolov3-tiny.py
@@ -11,7 +11,7 @@ import core.wrapper as nn
 def _create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--command_path',  type=str, default='./intuitus_commands/yolov3-tiny-commands', help='path to intuitus command files')
-    parser.add_argument('--image',  type=str, default='./cam_data/dog.npz', help='path to input image')
+    parser.add_argument('--image',  type=str, default= './cam_data/fmap_out_2.npy') # './cam_data/room.npz', help='path to input image') #
     parser.add_argument('--output',  type=str, default='./fmap_out', help='path to output')
     parser.add_argument('--tiny', type=bool, default=True, help='is yolo-tiny or not')
     parser.add_argument('--size', type=int, default=416, help='define input size of export model')
@@ -19,16 +19,19 @@ def _create_parser():
     parser.add_argument('--score', type=float, default=0.25, help='score threshold')
     parser.add_argument('--use_cam', type=bool, default=False, help='use camera for input img')
     parser.add_argument('--use_fb', type=bool, default=False, help='write output to framebuffer')
-    parser.add_argument('--print', type=bool, default=False, help='print network')
-    parser.add_argument('--execute', type=bool, default=True, help='print network')
+    parser.add_argument('--print', action='store_true', help='print network')
+    parser.add_argument('--not_execute', action='store_true', help='print network')
+    parser.add_argument('--iterations', type=int, default=1, help='execution iterations')
     return parser.parse_args() 
 
 
-layer_nbr = 2
+layer_nbr = 1
 
 def main(flags):
+    print(flags)
+    #print(flags.print)
     print_last = flags.print
-    exectue_net = flags.execute 
+    exectue_net = not flags.not_execute 
     input_size = flags.size
     image_path = pathlib.Path(__file__).absolute().parent / flags.image
     command_path = pathlib.Path(__file__).absolute().parent / flags.command_path
@@ -46,24 +49,28 @@ def main(flags):
     elif '.npz' in flags.image:
         img_npz = np.load(str(image_path),allow_pickle=True)
         image_data = img_npz['img']
-
+    elif '.npy' in flags.image:
+        image_data = np.load(str(image_path),allow_pickle=True)
+        print(image_data.shape)
     else:
+        print(image_path)
         original_image = cv2.imread(str(image_path))
         image_data = cv2.resize(original_image, (input_size, input_size))
         b,g,r = cv2.split(image_data)
         image_data = np.stack([r,g,b]).astype(np.uint8)
 
     Net = nn.Sequential(command_path)
-    buffer = Net.input(3,input_size,input_size)
+    #buffer = Net.input(3,input_size,input_size)
+    buffer = Net.input(32,104,104)
 
     #%% backbone 
-    buffer = Net.conv2d(buffer,16,(3,3),max_pooling=True,command_file='conv2d_0.npz') #208
-    # buffer = Net.conv2d(buffer,32,(3,3),max_pooling=True,command_file='conv2d_1.npz') #104
-    # buffer = Net.conv2d(buffer,64,(3,3),max_pooling=True,command_file='conv2d_2.npz') #52
+    #buffer = Net.conv2d(buffer,16,(3,3),max_pooling=True,command_file='conv2d_0.npz') #208
+    #buffer = Net.conv2d(buffer,32,(3,3),max_pooling=True,command_file='conv2d_1.npz') #104
+    buffer = Net.conv2d(buffer,64,(3,3),max_pooling=True,command_file='conv2d_2.npz') #52
     # buffer = Net.conv2d(buffer,128,(3,3),max_pooling=True,command_file='conv2d_3.npz') #26
     # buffer = Net.conv2d(buffer,256,(3,3),command_file='conv2d_4.npz') #26
     # route_1 = buffer
-    # buffer = Net.maxpool2d(buffer,pool_size=(2, 2), strides=(2,2)) # Software pooling (CPU)
+    #buffer = Net.maxpool2d(buffer, strides=(2,2)) # Software pooling (CPU)
     # buffer = Net.conv2d(buffer,512,(3,3),command_file='conv2d_5.npz')
     # buffer = Net.maxpool2d(buffer,pool_size=(2, 2), strides=(1,1)) # Software pooling (CPU)
     # buffer = Net.conv2d(buffer,1024,(3,3),command_file='conv2d_6.npz')
@@ -92,7 +99,8 @@ def main(flags):
     if print_last:
         Net.print_layer_dma_info(layer_nbr)
     if exectue_net:    
-        res = Net(image_data)
+        for i in range(flags.iterations):
+            res = Net(image_data)
         if out_path.exists():
             shutil.rmtree(out_path, ignore_errors=False, onerror=None)    
         out_path.mkdir(parents=True, exist_ok=True)    
